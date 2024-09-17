28fa299ed5ccea4b20d99c57cb12a3b768ee7802
Thelilrex <137387550+Thelilrex@users.noreply.github.com>
2024-09-17 23:55:05 +0900

    Second commit: Added Testing cases and updated ReadMe.docx file

--- src/main/java/TestUnit_Assignment.java
+++ src/main/java/TestUnit_Assignment.java
@@ -16,32 +16,24 @@ public class TestUnit_Assignment
     {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the desired string");
-        String user_Input = input.nextLine(); //Rule number one "Dont press enter"
+        String user_Input = input.nextLine(); 
         
         int char_Num = char_Count(user_Input);
         int vowel_Num = vowel_Count(user_Input);
         int digit_Num = digit_Count(user_Input);
-        
-        if((char_Num == -1) || (vowel_Num == -1) || (digit_Num == -1))
-        {
-            System.out.println("Its an empty string");
-        }
-        else
-        {
-            System.out.println("The character count is: " + char_Num);
-            System.out.println("The vowel count is: " + vowel_Num);
-            System.out.println("The digit count is: " + digit_Num);
-        }
+        System.out.println("The character count is: " + char_Num);
+        System.out.println("The vowel count is: " + vowel_Num);
+        System.out.println("The digit count is: " + digit_Num);
     }
     
     public static int char_Count(String input)
     {
+        if((input == null) || (input.isBlank()))
+        {
+            return 0;
+        }
         int size = input.length();
         int result = 0;
-        if(size == 0)
-        {
-            return -1;
-        }
         
         for (int i = 0; i < size; i++)
         {
@@ -55,12 +47,12 @@ public class TestUnit_Assignment
     }
     public static int vowel_Count(String input)
     {
+        if((input == null) || (input.isBlank()))
+        {
+            return 0;
+        }
         int size = input.length();
         int result = 0;
-        if(size == 0)
-        {
-            return -1;
-        }
         
         for (int i = 0; i < size; i++)
         {
@@ -83,12 +75,12 @@ public class TestUnit_Assignment
     }
     public static int digit_Count(String input)
     {
+        if((input == null) || (input.isBlank()))
+        {
+            return 0;
+        }
         int size = input.length();
         int result = 0;
-        if(size == 0)
-        {
-            return -1;
-        }
         
         for (int i = 0; i < size; i++)
         {
--- src/test/java/TestUnit_AssignmentTest.java
+++ src/test/java/TestUnit_AssignmentTest.java
@@ -11,62 +11,115 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Anuraag Reddy
  */
 public class TestUnit_AssignmentTest {
-    
-    public TestUnit_AssignmentTest() {
-    }
-
-    /**
-     * Test of main method, of class TestUnit_Assignment.
-     */
+    // Tests for the Characters
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        TestUnit_Assignment.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCharCountSpacesOnly() 
+    {
+        assertEquals(0, TestUnit_Assignment.char_Count("     "), "String with only spaces should return 0");
     }
-
-    /**
-     * Test of char_Count method, of class TestUnit_Assignment.
-     */
     @Test
-    public void testChar_Count() {
-        System.out.println("char_Count");
-        String input = "";
-        int expResult = 0;
-        int result = TestUnit_Assignment.char_Count(input);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCharCountEmptyString() 
+    {
+        assertEquals(0, TestUnit_Assignment.char_Count(""), "Empty string should return 0");
     }
-
-    /**
-     * Test of vowel_Count method, of class TestUnit_Assignment.
-     */
     @Test
-    public void testVowel_Count() {
-        System.out.println("vowel_Count");
-        String input = "";
-        int expResult = 0;
-        int result = TestUnit_Assignment.vowel_Count(input);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCharCountNull() 
+    {
+        assertEquals(0, TestUnit_Assignment.char_Count(null), "Null should return 0");
     }
-
-    /**
-     * Test of digit_Count method, of class TestUnit_Assignment.
-     */
     @Test
-    public void testDigit_Count() {
-        System.out.println("digit_Count");
-        String input = "";
-        int expResult = 0;
-        int result = TestUnit_Assignment.digit_Count(input);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCharCountAlphaNumerics() 
+    {
+        String input = "1a2b3c";
+        assertEquals(input.length(), TestUnit_Assignment.char_Count(input), "String with alphaNumerics with no space should return strings len");
+    }
+    @Test
+    public void testCharCountWithSpecialCharacters() 
+    {
+        String input = "!@#$%^&*";
+        assertEquals(input.length(), TestUnit_Assignment.char_Count(input), "String with special characters and no space should return its len");
+    }
+    @Test
+    public void testCharWithForeignCharacters()
+    {
+        String input = "こんにちは";
+        assertEquals(input.length(), TestUnit_Assignment.char_Count(input), "String with foreign characters with no space should return its len");
     }
     
+    //Tests for the Vowels
+    @Test
+    public void testVowelCountSpacesOnly() 
+    {
+        System.out.println("Supp");
+        assertEquals(0, TestUnit_Assignment.vowel_Count("     "), "String with only spaces should return 0");
+    }
+    @Test
+    public void testVowelCountEmptyString() 
+    {
+        assertEquals(0, TestUnit_Assignment.vowel_Count(""), "Empty string should return 0");
+    }
+    @Test
+    public void testVowelCountNull() 
+    {
+        assertEquals(0, TestUnit_Assignment.vowel_Count(null), "Null should return 0");
+    }
+    @Test
+    public void testVowelCountAlphaNumerics() 
+    {
+        String input = "1a2b3c";
+        assertEquals(1, TestUnit_Assignment.vowel_Count(input), "String with alphaNumerics should return number of vowels");
+    }
+    @Test
+    public void testVowelCountWithSpecialCharacters() 
+    {
+        String input = "!@a#$%^&*12";
+        assertEquals(1, TestUnit_Assignment.vowel_Count(input), "String with special should return number of vowels");
+    }
+    @Test
+    public void testVowelWithForeignCharacters()
+    {
+        String input = "こんにちは";
+        assertEquals(0, TestUnit_Assignment.vowel_Count(input), "String with foreign characters should return 0");
+    }
+    @Test
+    public void testVowelWithCaptialAndSmallVowels()
+    {
+        String input = "AaEeIiOoUu";
+        assertEquals(input.length(), TestUnit_Assignment.vowel_Count(input), "String with all capital and small vowels should return its len");
+    }
+    
+    // Test for digits
+     @Test
+    public void testDigitCountSpacesOnly() 
+    {
+        assertEquals(0, TestUnit_Assignment.digit_Count("     "), "String with only spaces should return 0");
+    }
+    @Test
+    public void testDigitCountEmptyString() 
+    {
+        assertEquals(0, TestUnit_Assignment.digit_Count(""), "Empty string should return 0");
+    }
+    @Test
+    public void testDigitCountNull() 
+    {
+        assertEquals(0, TestUnit_Assignment.digit_Count(null), "Null should return 0");
+    }
+    @Test
+    public void testDigitCountAlphaNumerics() 
+    {
+        String input = "1a2b3c";
+        assertEquals(3, TestUnit_Assignment.digit_Count(input), "String with alphaNumerics should return number of digits");
+    }
+    @Test
+    public void testDigitCountWithSpecialCharacters() 
+    {
+        String input = "!@#$%^&*";
+        assertEquals(0, TestUnit_Assignment.digit_Count(input), "String with special characters should return number of digits");
+    }
+    @Test
+    public void testDigitWithForeignCharacters()
+    {
+        String input = "こんにちは";
+        assertEquals(0, TestUnit_Assignment.digit_Count(input), "String with foreign characters should return number of digits");
+    }
 }
